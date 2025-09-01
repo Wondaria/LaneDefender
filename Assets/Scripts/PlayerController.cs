cc98ac0bda834bef1b63a4b1503f6ac84b9be118
Wondaria <mspangler@mail.bradley.edu>
2025-09-01 12:11:19 -0500

    Github

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+
+    private InputAction restart;
+    private InputAction quit;
+    private InputAction move;
+    private InputAction shoot;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
